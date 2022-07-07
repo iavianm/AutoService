324720e213a14b69fd6bdf931cee850b0cc51a09
iavianm <iavianm@mail.ru>
2022-07-07 12:49:00 +0300

    add category

---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+class Category < ApplicationRecord
+
+  validates :title, presence: true, length: {minimum: 2}
+
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+end
